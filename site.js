b04d8815b3a86a4858469be3a5e12c8e775db116
Gerardo Duino <gerardoduino@MacBook-Air-de-Gera.local>
2022-04-14 19:48:33 -0300

    primer desafio

---
+++ site.js
@@ -0,0 +1,6 @@
+
+var peso, altura, imc;
+peso = prompt ("cual es tu peso?");
+altura = prompt ("cual es tu altura?");
+imc = (parseInt (altura) * parseInt (altura) / parseInt (peso));
+alert ("tu imc es : " +imc);
